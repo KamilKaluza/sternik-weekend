4a94ce630df9f6485fbdd3e79722b1fc2a6bdabc
Kamil <Kamil@laptop>
2017-05-10 18:01:26 +0200

    poprawka

--- src/main/java/pl/sternik/kk/weekend/SpringBusinessConfig.java
+++ src/main/java/pl/sternik/kk/weekend/SpringBusinessConfig.java
@@ -11,8 +11,8 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ComponentScan(basePackages = { "pl.sternik.kk.week" }, excludeFilters = {
-        @Filter(type = FilterType.REGEX, pattern = "pl\\.sternik\\.kk\\.week\\.web\\..*") })
+@ComponentScan(basePackages = { "pl.sternik.kk.weekend" }, excludeFilters = {
+        @Filter(type = FilterType.REGEX, pattern = "pl\\.sternik\\.kk\\.weekend\\.web\\..*") })
 //@ImportResource({"classpath:/applicationContext.xml"})//,"classpath:/database-config.xml"})
 public class SpringBusinessConfig {
 
